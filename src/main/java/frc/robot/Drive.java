18595204b8ec66589bf6ca3bdd57fc63cac9366d
Saxena <krishna.saxena21@bcp.org>
2019-10-22 17:51:37 -0700

    added CAN Spark max motors to Drive

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -2,6 +2,8 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 
 
 public class Drive extends Subsystem {
@@ -9,6 +11,11 @@ public class Drive extends Subsystem {
     private TalonSRX leftS;
     private TalonSRX rightM;
     private TalonSRX rightS;
+
+    private CANSparkMax lSparkM;
+    private CANSparkMax lSparkS;
+    private CANSparkMax rSparkM;
+    private CANSparkMax rSparkS;
 
     private static final Drive mDrive = new Drive();
     private static PeriodicIO mPerIO = new PeriodicIO();
@@ -18,16 +25,30 @@ public class Drive extends Subsystem {
     }
 
     private Drive() {
+        //initialization of motors
         leftM = new TalonSRX(0);
         leftS = new TalonSRX(1);
         rightM = new TalonSRX(2);
         rightS = new TalonSRX(3);
 
+        lSparkM = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
+        lSparkS = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+        rSparkM = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+        rSparkS = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+        //invert right motors
         rightM.setInverted(true);
         rightS.setInverted(true);
 
+        rSparkM.setInverted(true);
+        rSparkS.setInverted(true);
+
+        //set slave motors
         leftS.set(ControlMode.Follower, 0);
         rightS.set(ControlMode.Follower, 2);
+
+        lSparkS.follow(lSparkM);
+        rSparkS.follow(rSparkM);
     }
 
     @Override
@@ -63,6 +84,9 @@ public class Drive extends Subsystem {
     public void writePeriodicOutputs() {
         leftM.set(ControlMode.PercentOutput, mPerIO.left_demand);
         rightM.set(ControlMode.PercentOutput, mPerIO.right_demand);
+
+        lSparkM.set(mPerIO.left_demand);
+        rSparkM.set(mPerIO.right_demand);
     }
 
     private static class PeriodicIO {
